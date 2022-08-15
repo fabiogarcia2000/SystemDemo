87138b859efa8f8e008d4c7969c7fe2db2c83005
eaplicano <eliud.aplicano@unah.hn>
2022-08-15 17:13:34 -0600

    Navegacion

--- js/Master.js
+++ js/Master.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+    
     AgregarMenu();
-    AgregarFooter();
+    
+    AgregarHeader();
 });
 
 function AgregarMenu(){
@@ -17,14 +19,14 @@ function AgregarMenu(){
         '</button>'+
         '<div class="collapse show" id="home-collapse">'+
           '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
-            '<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Sucursales</a></li>'+
+            '<li><a href="../forms/sucursal_lista.html" class="link-dark d-inline-flex text-decoration-none rounded">Sucursales</a></li>'+
 			'<li><a href="../forms/articulo_lista.html" class="link-dark d-inline-flex text-decoration-none rounded">Aritculos</a></li>'+
-			'<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Categorias</a></li>'+
+			'<li><a href="../forms/categoria_listar.html" class="link-dark d-inline-flex text-decoration-none rounded">Categorias</a></li>'+
             '<li><a href="../forms/impuesto_lista.html" class="link-dark d-inline-flex text-decoration-none rounded">Impuestos</a></li>'+
             '<li><a href="../forms/descuento_lista.html" class="link-dark d-inline-flex text-decoration-none rounded">Descuentos</a></li>'+
-			'<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Metodo de pago</a></li>'+
+			'<li><a href="../forms/metodo_pago_lista.html" class="link-dark d-inline-flex text-decoration-none rounded">Metodo de pago</a></li>'+
 			'<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Modo Pedido</a></li>'+
-			'<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Permisos SAR</a></li>'+
+			'<li><a href="../forms/permiso-sar.html" class="link-dark d-inline-flex text-decoration-none rounded">Permisos SAR</a></li>'+
 			'<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">POS</a></li>'+
 			'<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Mesas</a></li>'+
 		  '</ul>'+
@@ -34,12 +36,12 @@ function AgregarMenu(){
         '<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">'+
           'Inventario'+
         '</button>'+
-        '<div class="collapse" id="dashboard-collapse">'+
+        '<div class="collapse show" id="dashboard-collapse">'+
           '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
-            '<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Centro de Costo</a></li>'+
-			'<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Aritculos</a></li>'+
-            '<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Lista de Materiales</a></li>'+
-            '<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Unidades de Medida</a></li>'+
+            '<li><a href="../forms/centro_costo.html" class="link-dark d-inline-flex text-decoration-none rounded">Centro de Costo</a></li>'+
+			'<li><a href="../forms/articulo_lista.html" class="link-dark d-inline-flex text-decoration-none rounded">Aritculos</a></li>'+
+            '<li><a href="../forms/lista-materiales.html" class="link-dark d-inline-flex text-decoration-none rounded">Lista de Materiales</a></li>'+
+            '<li><a href="../forms/Unidades_medida_lista.html" class="link-dark d-inline-flex text-decoration-none rounded">Unidades de Medida</a></li>'+
 		'</ul>'+
         '</div>'+
       '</li>'+
@@ -47,7 +49,7 @@ function AgregarMenu(){
         '<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">'+
           'Reportes'+
         '</button>'+
-        '<div class="collapse" id="orders-collapse">'+
+        '<div class="collapse show" id="orders-collapse">'+
           '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
             '<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Venta Resumen</a></li>'+
 			'<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Venta Cajero</a></li>'+
@@ -62,7 +64,7 @@ function AgregarMenu(){
         '<button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">'+
           'Cuenta'+
         '</button>'+
-        '<div class="collapse" id="account-collapse">'+
+        '<div class="collapse show" id="account-collapse">'+
           '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'+
             '<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Perfil</a></li>'+
             '<li><a href="#" class="link-dark d-inline-flex text-decoration-none rounded">Ajustes</a></li>'+
@@ -79,4 +81,32 @@ function AgregarMenu(){
 function AgregarFooter(){
     var menu = document.getElementById("Pie");
     menu.innerHTML = '<div></div>'
+}
+
+function AgregarHeader(){
+  var menu = document.getElementById("Cabecera");
+    menu.innerHTML = '<header>'+
+    '<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #e43e3e ;">'+
+        '<div class="container-fluid">'+
+          '<a class="navbar-brand" href="#">'+
+            '<img src="../images/burridogs_logo.png" alt="" width="120" height="60" class="d-inline-block align-text-mid">'+
+            'Burridogs'+
+          '</a>'+
+          '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
+            '<span class="navbar-toggler-icon"></span>'+
+          '</button>'+
+          '<div class="collapse navbar-collapse" id="navbarSupportedContent">'+
+            '<ul class="navbar-nav mr-auto">'+
+              '<li class="nav-item dropdown">'+
+                '<div class="dropdown-menu" aria-labelledby="navbarDropdown">'+
+                  '<a class="dropdown-item" href="/ListaMats_Lista.html">Lista Materiales</a>'+
+                  '<a class="dropdown-item" href="/Metd_pago_lista.html">Metodo Pago</a>'+
+                  '<a class="dropdown-item" href="/Unidades_medida_lista.html">Unidades Medida</a>'+
+                '</div>'+
+              '</li>'+
+            '</ul>'+
+          '</div>'+
+        '</div>'+
+        '</nav>'+
+'</header>'
 }
